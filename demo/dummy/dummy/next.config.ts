3dde57760db57ac4cc7af4ead65a38e3550cdab2
vanha777 <vanha777@icloud.com>
2025-03-20 15:31:51 +1100

    x

--- demo/dummy/dummy/next.config.ts
+++ demo/dummy/dummy/next.config.ts
@@ -2,13 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  output: "standalone",
 };
 
 export default nextConfig;
